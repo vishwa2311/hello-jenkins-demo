c145b79f025d1da89353c8e7d3f2bb462791f1ac
vishwa2311 <49936518+vishwa2311@users.noreply.github.com>
2025-11-04 20:44:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   if (req.url === '/health') return res.end('OK');
-  res.end('Hello from Jenkins demo - Part 2!');
+  res.end('Hello from Jenkins demo - Part 3!');
 });
 server.listen(port, () => console.log('Server running on', port));
